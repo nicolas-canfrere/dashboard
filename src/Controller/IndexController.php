a31d2e87c3490f357e3aa1f639e19c14214190fb
nicolas <nicolas@samaritan.home>
2020-09-19 12:58:45 +0200

    save

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,11 +9,44 @@ class IndexController extends AbstractController
 {
     public function index()
     {
-        return $this->render('Index/index.html.twig');
+        return $this->render(
+            'Index/index.html.twig',
+            [
+                'users' => $this->generateUsers(15)
+            ]
+        );
     }
 
-    public function edit()
+    public function edit($id)
     {
-        return $this->render('Index/edit.html.twig');
+        $users = $this->generateUsers(15);
+        if (empty($users[$id])) {
+            throw $this->createNotFoundException();
+        }
+        return $this->render(
+            'Index/edit.html.twig',
+            [
+                'user' => $users[$id]
+            ]
+        );
+    }
+
+    private function generateUsers($number)
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+        $users = [];
+        for ($i = 0; $i < $number; $i++) {
+            $firstName = $faker->firstName;
+            $lastName  = $faker->lastName;
+            $users[]   = [
+                'firstName' => $firstName,
+                'lastName'  => $lastName,
+                'initials' => sprintf('%s%s', $firstName[0], $lastName[0]),
+                'email'     => $faker->safeEmail,
+                'activated' => $faker->boolean(40),
+            ];
+        }
+
+        return $users;
     }
 }
